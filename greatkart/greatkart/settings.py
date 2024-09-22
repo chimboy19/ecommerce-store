f3ba0462c6c6e4e559095235422b8e0bf84f51c9
Your Name <chimanancy16@gmail.com>
2024-09-22 04:56:41 -0700

    hidden 2

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -149,13 +149,6 @@ MESSAGE_TAGS = {
 }
 
 # smtp Configuration
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST='smtp.gmail.com'
-# EMAIL_PORT=587
-# EMAIL_HOST_USER='nicholasjones611@gmail.com'
-# EMAIL_HOST_PASSWORD='yhyx sasu bjbr sbrv '
-# EMAIL_USE_TLS=True
-# EMAIL_USE_SSL = False
 
 import os
 from dotenv import load_dotenv
