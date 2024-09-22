c6415f5738400e4d1b3347c72d778b9e0261f9e6
Your Name <chimanancy16@gmail.com>
2024-09-22 04:15:47 -0700

    hidden credentials

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -150,9 +150,23 @@ MESSAGE_TAGS = {
 
 # smtp Configuration
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER='nicholasjones611@gmail.com'
-EMAIL_HOST_PASSWORD='yhyx sasu bjbr sbrv '
-EMAIL_USE_TLS=True
+# EMAIL_HOST='smtp.gmail.com'
+# EMAIL_PORT=587
+# EMAIL_HOST_USER='nicholasjones611@gmail.com'
+# EMAIL_HOST_PASSWORD='yhyx sasu bjbr sbrv '
+# EMAIL_USE_TLS=True
 # EMAIL_USE_SSL = False
+
+import os
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+# Email settings from the environment variables
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
