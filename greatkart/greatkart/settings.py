750a748411308dc00d38a51d0597e804a53490e9
Your Name <chimanancy16@gmail.com>
2024-11-22 12:20:32 -0800

    changes new

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -154,9 +154,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIR=['static/']
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
-STATIC_ROOT =BASE_DIR/'staticfiles'
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_DIRS = [BASE_DIR / "static", '/var/www/static/',]
 
