58ef89dd6b383d4d320309c151ee1ae69b056d33
Your Name <chimanancy16@gmail.com>
2024-11-22 09:34:29 -0800

    fixed

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -191,7 +191,7 @@ load_dotenv()
 # EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool) 
 
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT', cast=int)
+EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') 
